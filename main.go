1868e72bfe6d53357f7afc6dfe46f8c7c97f6edc
Behrooz payoon <behrooz.payoon@gmail.com>
2019-01-14 10:35:59 +0330

    Add 404 header

--- main.go
+++ main.go
@@ -1,20 +1,21 @@
 package main
 
 import (
-  "fmt"
-  "net/http"
+	"fmt"
+	"net/http"
 )
 
 func handlerFunc(w http.ResponseWriter, r *http.Request) {
-  w.Header().Set("Content-Type", "text/html")
-  if r.URL.Path == "/" {
-    fmt.Fprint(w, "<h1>Home page</h1>")
-  }else {
-    fmt.Fprint(w, "<h1>Some other page</h1>")
-  } 
+	w.Header().Set("Content-Type", "text/html")
+	if r.URL.Path == "/" {
+		fmt.Fprint(w, "<h1>Home page</h1>")
+	} else {
+		w.WriteHeader(http.StatusNotFound)
+		fmt.Fprint(w, "<h1>404</h1>")
+	}
 }
 
 func main() {
-  http.HandleFunc("/", handlerFunc)
-  http.ListenAndServe(":3000",nil)
+	http.HandleFunc("/", handlerFunc)
+	http.ListenAndServe(":3000", nil)
 }
