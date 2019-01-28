73328495c211382e916cb8d8ab99c70986a1cd68
Behrooz payoon <behrooz.payoon@gmail.com>
2019-01-28 10:57:35 +0330

    Add static controller

--- main.go
+++ main.go
@@ -1,54 +1,19 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
 
 	"github.com/gorilla/mux"
 	"github.com/photoApp/controllers"
-	"github.com/photoApp/views"
 )
-
-var (
-	homeView    *views.View
-	contactView *views.View
-)
-
-func handlerFunc(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/html")
-	if r.URL.Path == "/contact" {
-		fmt.Fprint(w, "<h1>Contact page</h1>")
-	} else {
-		w.WriteHeader(http.StatusNotFound)
-		fmt.Fprint(w, "<h1>404</h1>")
-	}
-}
-
-func home(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/html")
-	must(homeView.Render(w, nil))
-}
-
-func contact(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/html")
-	must(contactView.Render(w, nil))
-}
-
-func notFound(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/html")
-	w.WriteHeader(http.StatusNotFound)
-	fmt.Fprintf(w, "<h1>404 custom page</h1>")
-}
 
 func main() {
-	homeView = views.NewView("bootstrap", "views/home.gohtml")
-	contactView = views.NewView("bootstrap", "views/contact.gohtml")
+	staticController := controllers.NewStatic()
 	usersController := controllers.NewUsers()
 
 	router := mux.NewRouter()
-	router.NotFoundHandler = http.HandlerFunc(notFound)
-	router.HandleFunc("/", home).Methods("GET")
-	router.HandleFunc("/contact", contact).Methods("GET")
+	router.Handle("/", staticController.Home).Methods("GET")
+	router.Handle("/contact", staticController.Contact).Methods("GET")
 	router.HandleFunc("/signup", usersController.New).Methods("GET")
 	router.HandleFunc("/signup", usersController.Create).Methods("POST")
 	http.ListenAndServe(":3000", router)
